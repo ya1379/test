b35f81348ec6ca69bbe4a8abccdff8cdf274922c
ya1379 <ya1379@gmail.com>
2016-06-03 03:09:00 +0000

    static page done!

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      get static_pages_home_path
+      expect(response).to have_http_status(200)
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+  end
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      get static_pages_help_path
+      expect(response).to have_http_status(200)
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+  end
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      get static_pages_about_path
+      expect(response).to have_http_status(200)
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
+    end
+  end
+end

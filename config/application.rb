0263cf0668e0aa539845710bce26e973d219939e
ya1379 <ya1379@gmail.com>
2016-06-07 08:31:36 +0000

    verification done

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module TestApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = true
   end
 end

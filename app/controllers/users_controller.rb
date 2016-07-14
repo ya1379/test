920e97cafaf8e0b2c44e71644e4649fddf60ad14
ya1379 <ya1379@gmail.com>
2016-07-14 05:41:59 +0000

    lastone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[user_params])    # Not the final implementation!
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
       # Handle a successful save.
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 TestApp::Application.routes.draw do
   root  'static_pages#home'
+  resources :users
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
